5e2ac03b7869ab37b489ef759de783aaf6fa6629
tranmanhduy <n22dccn114@student.ptithcm.edu.vn>
2025-02-24 12:27:40 +0700

    update loss function

--- loss.py
+++ loss.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class LossFunction(nn.Module):
     def __init__(self):
         super(LossFunction, self).__init__()
-        self.mse = nn.MSELoss(reduction="sum")
+        self.mse = nn.MSELoss(reduction="mean")
         self.lambda_loss_true = 5.0
     
     def forward(self, predictions, targers):
@@ -12,7 +12,7 @@ class LossFunction(nn.Module):
         predictions_true = predictions * targers
         predictions_false = (1 - targers) * predictions
         
-        loss_pred_true = torch.sum(predictions_true)
-        loss_pred_false = torch.sum(predictions_true)
+        loss_pred_true = torch.abs(torch.sum(predictions_true))
+        loss_pred_false = torch.abs(torch.sum(predictions_true))
         
         return loss_pred_false + loss_pred_true * self.lambda_loss_true
